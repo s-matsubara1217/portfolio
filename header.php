3cc60658a31f30cc6e34afbb1b843eafd8298757
s-matsubara <shohs12171217@gmail.com>
2024-01-15 12:43:01 +0900

    240115-2

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 </script>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-<title>SHO MATSUBARA | ポートフォリオサイト</title>
+<title>SHO MATSUBARA | ポートフォリオサイトaa</title>
 
 <meta name="description" content="" />
 <meta name="keywords" content="" />
